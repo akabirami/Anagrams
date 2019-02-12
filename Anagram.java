bd949df42ed7a419113c210975e64517f1cc8579
A K <abiak@paypal.com>
2019-02-12 21:20:30 +0530

    Commit 1

--- Anagram.java
+++ Anagram.java
@@ -24,7 +24,9 @@ public class Anagram {
 		String clusters="";
 		for(String s: anagram.keySet()){
 			if(anagram.get(s).contains(" ")){
+        // this is also a check
 				clusters+=anagram.get(s)+"\n";
+        // this is just to check
 			}
 		}
 		return clusters;
